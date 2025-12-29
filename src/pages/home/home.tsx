971f5e200355659c21f65a4193fa15d1d286f4f1
Yusuf Kaya <yusuf.kaya1059@gmail.com>
2025-12-29 21:53:29 +0300

    CSS codes have been revized

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,11 @@
 import Welcome from "@components/welcome/welcome";
+import ArticleCard from "@/components/Article/ArticleCard";
+import { articleCards } from "@/constants/constants";
 
 function Home() {
-  return <div></div>;
+  return <div>
+    {articleCards.map((card, index) => <ArticleCard key={index} {...card} />)}
+  </div>;
 }
 
 export default Home;
