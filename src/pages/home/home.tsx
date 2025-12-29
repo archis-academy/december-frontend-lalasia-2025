0ee00b02867547bb7bc4c93ee264424d587f5234
Yusuf Kaya <yusuf.kaya1059@gmail.com>
2025-12-29 21:54:07 +0300

    revized-2

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,7 @@
 import Welcome from "@components/welcome/welcome";
-import ArticleCard from "@/components/Article/ArticleCard";
-import { articleCards } from "@/constants/constants";
 
 function Home() {
-  return <div>
-    {articleCards.map((card, index) => <ArticleCard key={index} {...card} />)}
-  </div>;
+  return <div></div>;
 }
 
 export default Home;
