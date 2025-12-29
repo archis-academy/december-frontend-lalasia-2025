3e251b75cc5b6e96c8f3294ca3bdf3fc0a063a35
palaogluserdest <palaogluserdest@gmail.com>
2025-12-29 19:56:04 +0300

    Add useData to get data on mockapi

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,18 @@
+"use server"
+
+import { useData } from "@/hooks/useData";
 import Welcome from "@components/welcome/welcome";
+import { use } from "react";
 
 function Home() {
+
+
+  const { data } = useData({ endpoint: "products" });
+
+
+  console.log(data);
+
+
   return <div>Home page</div>;
 }
 
