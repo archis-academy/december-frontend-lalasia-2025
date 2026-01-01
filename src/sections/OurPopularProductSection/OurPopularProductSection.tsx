49f89d133b6b7ca07c9819526884e52000896bc5
Gulizar Korbulak <korbulakgulizar@gmail.com>
2026-01-01 16:21:59 +0100

    Card yerleri olusturuldu, slider aktif fakat genis ekrandaki görüntüde problem var

--- src/sections/OurPopularProductSection/OurPopularProductSection.tsx
+++ src/sections/OurPopularProductSection/OurPopularProductSection.tsx
@@ -8,9 +8,17 @@ import "swiper/css/pagination";
 
 // Modüller
 import { Navigation, Pagination } from "swiper/modules";
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+} from "../../../public/icons/OurProductSection/icons";
+
+import { useRef } from "react";
 
 const OurPopularProductSection = () => {
-  const blankCards = [1, 2, 3, 4, 5, 6, 7, 8]; // Placeholder for blank cards
+  const blankCards = [1, 2, 3, 4, 5, 6, 7, 8];
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
 
   return (
     <section className={styles.ourPopularProductSection}>
@@ -22,16 +30,20 @@ const OurPopularProductSection = () => {
         placerat nisi, adipiscing mauris non purus parturient.
       </p>
 
-      {/* --- SWIPER BURADA BAŞLIYOR --- */}
       <Swiper
         modules={[Navigation, Pagination]}
-        navigation
-        loop
         centeredSlides={true}
-        slidesPerView={2.5}
-        /*slidesPerView={"auto"}*/
+        loop
         spaceBetween={30}
         className={styles.swiperWrapper}
+        onBeforeInit={(swiper) => {
+          swiper.params.navigation.prevEl = prevRef.current;
+          swiper.params.navigation.nextEl = nextRef.current;
+        }}
+        navigation={{
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
         breakpoints={{
           320: {
             slidesPerView: 1.2,
@@ -42,8 +54,8 @@ const OurPopularProductSection = () => {
             spaceBetween: 20,
           },
           1024: {
-            slidesPerView: 2.5,
-            spaceBetween: 30,
+            slidesPerView: 3.5,
+            spaceBetween: 20,
           },
         }}
       >
@@ -55,6 +67,14 @@ const OurPopularProductSection = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className={styles.navButtonsWrapper}>
+        <button ref={prevRef} className={styles.customPrevBtn}>
+          <ArrowLeftIcon />
+        </button>
+        <button ref={nextRef} className={styles.customNextBtn}>
+          <ArrowRightIcon />
+        </button>
+      </div>
     </section>
   );
 };
