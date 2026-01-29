62bb2921255049884f043f84f75acb2cf1a17822
Yusuf Kaya <yusuf.kaya1059@gmail.com>
2026-01-29 23:30:55 +0300

    popular products section has been created as desktop design. responsive design will be added

--- src/components/PopularProducts/PopularProducts.tsx
+++ src/components/PopularProducts/PopularProducts.tsx
@@ -1,7 +1,7 @@
 import 'swiper/swiper-bundle.css';
 import styles from "./PopularProducts.module.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
+import { Navigation, Autoplay } from "swiper/modules";
 import ProductCard from "../ProductCard/ProductCard";
 import { useData } from "@/hooks/useData";
 import { type Product } from "@/types/types";
@@ -18,9 +18,6 @@ export default function PopularProducts() {
         },
         [data]
     );
-
-    console.log(popularProducts);
-
 
     if (isLoading) return <div>Loading...</div>
 
@@ -39,12 +36,15 @@ export default function PopularProducts() {
 
             <Swiper
                 loop={true}
-                autoplay={true}
-                navigation={true}
+                autoplay={{ delay: 2000 }}
+                navigation={{
+                    nextEl: `.${styles.swiperNextButton}`,
+                    prevEl: `.${styles.swiperPrevButton}`,
+                }}
                 spaceBetween={29}
                 centeredSlides={true}
                 slidesPerView={'auto'}
-                modules={[Navigation]}
+                modules={[Navigation, Autoplay]}
                 className={styles.popularSwiper}
             >
 
@@ -56,9 +56,19 @@ export default function PopularProducts() {
                     </SwiperSlide>
                 )}
 
+                <button className={styles.swiperPrevButton}>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.0898 19.92L8.56984 13.4C7.79984 12.63 7.79984 11.37 8.56984 10.6L15.0898 4.08" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                </button>
+
+                <button className={styles.swiperNextButton}>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.91016 19.92L15.4302 13.4C16.2002 12.63 16.2002 11.37 15.4302 10.6L8.91016 4.08" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                </button>
 
             </Swiper>
-
 
         </section>
     )
