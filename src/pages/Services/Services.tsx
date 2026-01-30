7835367d6398ffef2016d7bc5f57b37f505d9c9f
esrask319 <esrask319@icloud.com>
2026-01-30 22:27:06 +0300

    added features section

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -1,10 +1,12 @@
 import Portfolio from "./Portfolio/Portfolio"
 import ServicesHero from "./ServicesHero/ServicesHero"
+import Features from "./Features/Features"
 
 const Services = () => {
   return (
     <div>
      <ServicesHero />
+     <Features />
      <Portfolio/>
     </div>
   )
