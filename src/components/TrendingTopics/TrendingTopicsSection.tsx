aff12a829ab8d58214696b057cef745e2faafe3d
miray <miraynarin@users.noreply.github.com>
2026-02-23 15:15:22 +0300

    ArticleCard Component updated

--- src/components/TrendingTopics/TrendingTopicsSection.tsx
+++ src/components/TrendingTopics/TrendingTopicsSection.tsx
@@ -1,7 +1,53 @@
-import  { useState, useMemo, useRef, useEffect } from "react";
+import { useState, useMemo, useRef, useEffect } from "react";
 import type { ArticleCardProps } from "@/types/types";
 import { useData } from "@/hooks/useData";
 import style from "./TrendingTopics.module.scss";
+
+
+type ArticleCardComponentProps = {
+  article: ArticleCardProps;
+};
+
+const ArticleCard = ({ article }: ArticleCardComponentProps) => {
+  return (
+    <div className={style.articleCard}>
+      <img
+        src={article.image}
+        alt={article.title}
+        className={style.articleImage}
+      />
+
+      <div className={style.articleText}>
+        <span className={style.articleCategory}>
+          {article.category}
+        </span>
+
+        <h4 className={style.articleTitle}>
+          {article.title}
+        </h4>
+
+        <p className={style.articleContext}>
+          {article.context.slice(0, 150)}...
+        </p>
+
+        <div className={style.articleMeta}>
+          <img
+            src="/icons/trendingTopics/article-icon-user.svg"
+            alt="User Image"
+            className={style.articleIconUser}
+          />
+          <span className={style.authorName}>
+            By Morgan Goldberg
+          </span>
+          <span className={style.publishDate}>
+            Friday, 1 April 2022
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 
 const TrendingTopicsSection = () => {
   const { data, isLoading, error } = useData<ArticleCardProps[]>({
@@ -134,33 +180,7 @@ const TrendingTopicsSection = () => {
 
         <div className={style.articlesContainer}>
           {filteredSortedData.map((article) => (
-            <div key={article.id} className={style.articleCard}>
-              <img
-                src={article.image}
-                alt={article.title}
-                className={style.articleImage}
-              />
-              <div className={style.articleText}>
-                <span className={style.articleCategory}>
-                  {article.category}
-                </span>
-                <h4 className={style.articleTitle}>
-                  {article.title}
-                </h4>
-                <p className={style.articleContext}>
-                  {article.context.slice(0, 150)}...
-                </p>
-                <div className={style.articleMeta}>
-                  <img
-                src="/icons/trendingTopics/article-icon-user.svg"
-                alt="User Image"
-                className={style.articleIconUser}
-              />
-                <span className={style.authorName}>By Morgan Goldberg</span> 
-                <span className={style.publishDate}>Friday, 1 April 2022</span>
-                </div>
-              </div>
-            </div>
+            <ArticleCard key={article.id} article={article} />
           ))}
         </div>
       </div>
