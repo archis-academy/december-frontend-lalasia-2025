6708abea09eb60b2502a04222247a8c509e4c124
palaogluserdest <palaogluserdest@gmail.com>
2026-02-23 21:33:59 +0300

    Created TrendigTopicsCard component

--- src/components/TrendingTopics/TrendingTopicsSection.tsx
+++ src/components/TrendingTopics/TrendingTopicsSection.tsx
@@ -2,51 +2,10 @@ import { useState, useMemo, useRef, useEffect } from "react";
 import type { ArticleCardProps } from "@/types/types";
 import { useData } from "@/hooks/useData";
 import style from "./TrendingTopics.module.scss";
+import TrendingTopicCard from "./TrendingTopicCard";
 
 
-type ArticleCardComponentProps = {
-  article: ArticleCardProps;
-};
 
-const ArticleCard = ({ article }: ArticleCardComponentProps) => {
-  return (
-    <div className={style.articleCard}>
-      <img
-        src={article.image}
-        alt={article.title}
-        className={style.articleImage}
-      />
-
-      <div className={style.articleText}>
-        <span className={style.articleCategory}>
-          {article.category}
-        </span>
-
-        <h4 className={style.articleTitle}>
-          {article.title}
-        </h4>
-
-        <p className={style.articleContext}>
-          {article.context.slice(0, 150)}...
-        </p>
-
-        <div className={style.articleMeta}>
-          <img
-            src="/icons/trendingTopics/article-icon-user.svg"
-            alt="User Image"
-            className={style.articleIconUser}
-          />
-          <span className={style.authorName}>
-            By Morgan Goldberg
-          </span>
-          <span className={style.publishDate}>
-            Friday, 1 April 2022
-          </span>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 
 const TrendingTopicsSection = () => {
@@ -180,7 +139,7 @@ const TrendingTopicsSection = () => {
 
         <div className={style.articlesContainer}>
           {filteredSortedData.map((article) => (
-            <ArticleCard key={article.id} article={article} />
+            <TrendingTopicCard key={article.id} article={article} />
           ))}
         </div>
       </div>
